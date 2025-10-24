51767aefb6042823f1f8ee0522e1eab8baa40000
michel <michel.erard@gmail.com>
2025-10-24 12:54:33 +0200

    Update to latest spring boot version

--- src/main/java/ch/erard22/demo/ProductRepository.java
+++ src/main/java/ch/erard22/demo/ProductRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    List<Product> findByName(String Name);
+    List<Product> findByName(String name);
 
     Product findById(long id);
 
